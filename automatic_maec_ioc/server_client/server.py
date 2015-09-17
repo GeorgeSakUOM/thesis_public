05bc3af38e867801dc515b5749e76cf34333bd70
geosakel77 <mai1428@uom.edu.gr>
2015-09-18 00:00:21 +0300

    Cuckoo Messnger tested and functional

--- automatic_maec_ioc/server_client/server.py
+++ automatic_maec_ioc/server_client/server.py
@@ -7,18 +7,22 @@ print >> sys.stderr, 'starting up on %s port %s '% server_adress
 sock.bind(server_adress)
 sock.listen(1)
 
+dbfilename='cuckko_results'
 while True:
     print >>sys.stderr, 'waiting for a connection' 
     connection, client_address = sock.accept()
     try:
         print >>sys.stderr, 'connection from', client_address
-
+        #connection.send('200')
         # Receive the data in small chunks and retransmit it
+        dbfile =open(dbfilename,'a')
         while True:
+            
             data = connection.recv(16)
             print >>sys.stderr, 'received "%s"' % data
             if data:
-                print >>sys.stderr, 'sending data back to the client'
+                dbfile.write(data)
+                #print >>sys.stderr, 'sending data back to the client'
                 connection.sendall(data)
             else:
                 print >>sys.stderr, 'no more data from', client_address
@@ -27,6 +31,7 @@ while True:
     finally:
         # Clean up the connection
         connection.close()
+        dbfile.close()
 
  
 
